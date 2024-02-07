0922bafed201599c24ee75062d5898dd0f2151da
Joan <joan.vigne@hotmail.fr>
2024-02-07 13:17:40 +0000

    fix menu deroulant

--- src/components/SectionProjets.tsx
+++ src/components/SectionProjets.tsx
@@ -24,8 +24,31 @@ const SectionProjets = () => {
 
   useEffect(() => {
     const fetchFormations = async () => {
-      const fetched = await fetchDataFromDBToSessionStorage("projets");
-      setProjets(fetched[0]);
+      const fetched: ProjetData[] = await fetchDataFromDBToSessionStorage(
+        "projets"
+      );
+      console.log(fetched);
+      // trier par date :
+      const sortedProjets: ProjetData = Object.values(fetched[0]).sort(
+        (a, b) => {
+          // Vérifier si a.date et b.date sont définis
+          if (a.date && b.date) {
+            // Convertir les dates en objets Date
+            const [monthA, yearA] = a.date.split("/");
+            const [monthB, yearB] = b.date.split("/");
+            // Comparer les années
+            if (yearA !== yearB) {
+              return parseInt(yearB) - parseInt(yearA);
+            }
+            // Si les années sont égales, comparer les mois
+            return parseInt(monthB) - parseInt(monthA);
+          } else {
+            // Gérer le cas où a.date ou b.date est indéfini
+            return 0;
+          }
+        }
+      );
+      setProjets(sortedProjets);
     };
 
     fetchFormations();
